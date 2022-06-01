882261e22dcaa0fc1ec6ff2c8554a0d6a5cedb38
Jocelyn Naarden <tzionone@gmail.com>
2022-06-01 00:41:38 -0400

    ✅

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+  function eyesOnMeFocus() {
+    console.log('Good!')
+  }
+  function eyesOnMeBlur() {
+    console.log('Hey! Eyes on me!')
+  }
+  return (
+    <button onFocus={eyesOnMeFocus} onBlur={eyesOnMeBlur}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+
+function keyPadOnChange(e) {
+  console.log('Entering password...')
+}
+
+function Keypad() {
+  return (
+    <>
+      <input type="password" onChange={keyPadOnChange} />
+    </>
+  )
+}
+
+export default Keypad
